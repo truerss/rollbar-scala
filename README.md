330bb4ecb62150709e4599605747278b85dda457
mike <mike.fch1@gmail.com>
2016-03-10 00:30:33 +0300

    publish to bintray

--- README.md
+++ README.md
@@ -1,22 +1,28 @@
 # rollbar-scala
 
-Api access for [rollbar](https://rollbar.com/)
+Easy Api access for [rollbar](https://rollbar.com/)
 
 # How to
+
+add dependency
+
+`"com.github.truerss" %% "rollbar-scala" % "0.0.1"`
+
+and then
 
 ```scala
 import com.github.truerss.rollbar._
 
-val token = "my-access-token"
+val token = "you-access-token" // <- post_server_item
 
 val builder = NotifyBuilder(token, "environment")
-Sender.send(builder) // Result(0, "some-uuid")
+Sender.send(builder.info("some message")) // Result(0, "some-uuid")
 
 // or use own sender
 import spray.json._
 import com.github.truerss.rollbar.json._
 val builder = NotifyBuilder(token, "environment")
-MySender.send(builder.info("some-message").toJson)
+MySender.send(builder.info("some message").toJson)
 ```
 
 
--- project/RollbarBuild.scala
+++ project/RollbarBuild.scala
@@ -1,5 +1,8 @@
 import sbt._
 import sbt.Keys._
+import bintray.BintrayKeys.bintrayOrganization
+import bintray.BintrayPlugin._
+
 
 object RollbarBuild extends Build {
   val opts = Seq(
@@ -19,12 +22,31 @@ object RollbarBuild extends Build {
       name := "rollbar-scala",
       version := "0.0.1",
       licenses += ("MIT", url("http://opensource.org/licenses/MIT")),
+      crossScalaVersions := Seq("2.10.5", "2.11.6"),
+      publishMavenStyle := true,
+      bintrayOrganization := Some("truerss"),
+      pomExtra := pomXml,
+      publishArtifact in Test := false,
       libraryDependencies ++= Seq(
         "io.spray" %% "spray-json" % "1.3.2",
         "org.scalaj" %% "scalaj-http" % "2.2.1"
       )
     )
   )
+
+  val pomXml =
+    <url>https://github.com/truerss/rollbar-scala</url>
+      <scm>
+        <url>git@github.com:truerss/rollbar-scala.git</url>
+        <connection>scm:git:git@github.com:truerss/rollbar-scala.git</connection>
+      </scm>
+      <developers>
+        <developer>
+          <id>fntz</id>
+          <name>mike</name>
+          <url>https://github.com/fntzr</url>
+        </developer>
+      </developers>
 
 }
 
