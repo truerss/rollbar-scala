734d72b7040232b0f3692f53220256a88ee63dee
mike <mike.fch1@gmail.com>
2017-10-25 16:32:38 +0300

    #4 hide access token

--- src/main/scala/com/github/truerss/rollbar/Sender.scala
+++ src/main/scala/com/github/truerss/rollbar/Sender.scala
@@ -2,27 +2,32 @@ package com.github.truerss.rollbar
 
 import scalaj.http._
 import spray.json._
+import java.util.logging
 
 object Sender {
   import json._
   import entities.Result
-  import java.util.logging.Logger
+  import logging.Logger
 
-  def uuid = java.util.UUID.randomUUID().toString
+  private final val _timeout = 10000
 
-  final val _timeout = 10000
+  private final val hidden = Map("access_token" -> JsString("******"))
 
-  val log = Logger.getLogger(getClass.getName)
+  private val log = Logger.getLogger(getClass.getName)
+
+  private def uuid = java.util.UUID.randomUUID().toString
+
   def send(
            p: NotifyBuilder[Level],
            url: String = "https://api.rollbar.com/api/1/item/"
   )(implicit timeout: Int = _timeout) = {
     val u = uuid
-    val json = p.payload.toJson.toString
-    log.info(s"Send [$u]: $json to $url")
+    val json = p.payload.toJson
+    val hideAccessToken = JsObject(json.asJsObject.fields ++ hidden)
+    log.info(s"Send [$u]: $hideAccessToken to $url")
     val result = Http(url)
       .timeout(connTimeoutMs = _timeout, readTimeoutMs = _timeout)
-      .postData(json).asString.body.parseJson.convertTo[Result]
+      .postData(json.toString).asString.body.parseJson.convertTo[Result]
     log.info(s"Response [$u] given: $result")
     result
   }
